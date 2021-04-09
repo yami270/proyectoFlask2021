96beb28b6d79bb380240da27f8f2839466ffa372
yami270 <smma.fni@gmail.com>
2021-04-08 21:20:56 -0400

    Agregando login, logout y protegiendo las rutas

--- appPackage/__init__.py
+++ appPackage/__init__.py
@@ -3,7 +3,10 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
+# Agregando el login manager
 login_manager = LoginManager(app)
+login_manager.login_view = 'loginRoute'
+login_manager.init_app(app)
 
 from appPackage import models
 from appPackage import routes
--- appPackage/routes.py
+++ appPackage/routes.py
@@ -8,46 +8,73 @@ from flask_login import login_required, logout_user, current_user, login_user
 def load_user(user_id):
 	return user.query.get(user_id)
 
-@app.route('/')
-def index():
-	return redirect(url_for('loginRoute'))
-
 @app.route('/login', methods=["GET", "POST"])
 def loginRoute():
+	if current_user.is_authenticated:
+		return redirect(url_for('homeRoute'))
 	form = loginForm(request.form) # Creacion del formulario
 	if request.method == 'POST' and form.validate():
+		userTemp = db.session.query(user).filter(user.username == request.form['username']).first()
+		if not userTemp or not userTemp.check_password(request.form['password']):
+			flash('Error: No existe una cuenta con los datos ingresados', 'WA')
+			return render_template('login.html', form=form)
+		# inicio de session
+		login_user(userTemp)
+		flash('tdo bien', 'AC')
 		return redirect(url_for('homeRoute'))
 	return render_template('login.html', form=form)
 
-@app.route('/home')
+@app.route('/logout', methods=["POST"])
+@login_required
+def logoutRoute():
+	form = loginForm(request.form) # Creacion del formulario
+	logout_user()
+	return redirect(url_for('loginRoute'))
+
+@app.route('/', methods=["GET"])
+@app.route('/home', methods=["GET"])
+@login_required
 def homeRoute():
-	return render_template('home.html')
+	form = loginForm()
+	return render_template('home.html', formLogout=form, current_user=current_user)
 
 @app.route('/registro', methods=["GET", "POST"])
-def register():
+def registerRoute():
 	form = registerForm(request.form) # Creacion del formulario
 	if request.method == 'POST' and form.validate():
-		return 'esto es from de registro valido'
+		userTemp = db.session.query(user).filter(user.nameUser == request.form['nameUser']).first()
+		if userTemp: # error de nombre
+			flash('Error: Ya existe una cuenta registra a ese nombre', 'WA')
+			return render_template('register.html', form=form)
+		userTemp = db.session.query(user).filter(user.username == request.form['username']).first()
+		if userTemp: # error de nombre de usuario
+			flash('Error: Ya existe una cuenta registra a ese nombre de usuario', 'WA')
+			return render_template('register.html', form=form)
+		userNuevo = user(nameUser=request.form['nameUser'], username=request.form['username'], typeUser=request.form['typeUser'])
+		userNuevo.set_password(request.form['password'])
+		db.session.add(userNuevo)
+		db.session.commit() 
+		flash('Usuario registrado exitosamente', 'AC')
+		return redirect(url_for('homeRoute'))
 	else:
-		# userNuevo = user(nameUser='migufe6l4', username='yamri2d77_', typeUser=1)
-		# userNuevo.set_password('miau')
-		# db.session.add(userNuevo)
-		# db.session.commit()  # Create new user
-		# #login_user(userNuevo) #logea al nuevo usuario
 		return render_template('register.html', form=form)
 
-@app.route('/registroMaquina')
+@app.route('/registroMaquina', methods=["GET", "POST"])
+@login_required
 def registerMachineRoute():
 	return 'registrar maquina'
 
-@app.route('/registroComponente')
+@app.route('/registroComponente', methods=["GET", "POST"])
+@login_required
 def registerComponentRoute():
 	return 'registrar componente'
 
-@app.route('/actualizar')
+@app.route('/actualizar', methods=["GET", "POST"])
+@login_required
 def updateRoute():
 	return 'actualizar datos'
 
-@app.route('/stock')
+@app.route('/stock', methods=["GET", "POST"])
+@login_required
 def stockRoute():
 	return 'registrar stock'
